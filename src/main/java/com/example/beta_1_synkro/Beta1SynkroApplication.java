d0d170063815a176f1c7a355fb24faf17b7f29f4
Paulina Cuartas <paulinacuartas2005@gmail.com>
2026-09-18 06:25:46 -0500

    feat se crea el proyecto

---
+++ src/main/java/com/example/beta_1_synkro/Beta1SynkroApplication.java
@@ -0,0 +1,13 @@
+package com.example.beta_1_synkro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beta1SynkroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beta1SynkroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beta_1_synkro/Beta1SynkroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beta_1_synkro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beta1SynkroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
