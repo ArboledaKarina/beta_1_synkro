f547cec195a9c42a57be59068a25cdfd229e6e63
Karina Arboleda <arboledakarina15@gmail.com>
2026-09-18 08:15:31 -0500

    feat se crea modelo de Reto

---
+++ src/main/java/com/example/beta_1_synkro/models/Reto.java
@@ -0,0 +1,103 @@
+package com.example.beta_1_synkro.models;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "retos")
+public class Reto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nombre", nullable = false, length = 50)
+    private String nombre;
+
+    @Column(name = "descripcion", nullable = false, length = 50)
+    private String descripcion;
+
+    @Column(name = "fecha_inicio", nullable = false)
+    private LocalDate fecha_inicio;
+
+    @Column(name = "fecha_fin", nullable = false)
+    private LocalDate fecha_fin;
+
+    @Column(name = "estado", nullable = false, length = 50)
+    private String estado;
+
+    // Lado inverso con registro
+    @OneToMany(mappedBy = "reto")
+    private List<Registro> registros;
+    
+    //constructor vacío
+    public Reto() {
+    }
+
+    // Getters y Setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getFecha_inicio() {
+        return fecha_inicio;
+    }
+
+    public void setFecha_inicio(LocalDate fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+
+    public LocalDate getFecha_fin() {
+        return fecha_fin;
+    }
+
+    public void setFecha_fin(LocalDate fecha_fin) {
+        this.fecha_fin = fecha_fin;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public List<Registro> getRegistros() {
+        return registros;
+    }
+
+    public void setRegistros(List<Registro> registros) {
+        this.registros = registros;
+    }
+}
